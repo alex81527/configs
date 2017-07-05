1841f9abed80867d124e1444cdab6e76d7d25fd1
W. Alex Chen <alex81527@gmail.com>
2017-07-05 23:06:53 +0800

    Update vim-setup.sh

--- vim-setup.sh
+++ vim-setup.sh
@@ -21,15 +21,5 @@ rm -f tmprc
 echo 'Install powerline fonts for vim-airline plugin...'
 git clone https://github.com/powerline/fonts.git && sh fonts/install.sh
 
-#In some distributions, Terminess Powerline is ignored by default and must be 
-#explicitly allowed.
-if [ ! -d "$HOME/.config/fontconfig/conf.d" ]; then
-    mkdir -p "$HOME/.config/fontconfig/conf.d"
-fi
-
-echo 'cp fontconfig files for powerline fonts...'
-cp fonts/fontconfig/* "$HOME/.config/fontconfig/conf.d/"
-fc-cache -vf
-
 rm -rf ./fonts
 echo '================================================================='
